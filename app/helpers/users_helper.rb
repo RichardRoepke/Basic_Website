d18b8311f4c046ebcb8543ed48df85496c674c73
Richard Roepke <RAERoepke@gmail.com>
2018-07-09 15:55:58 -0400

    Fiddling with User Signups

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user)
+    link_to image_tag("rails.png", alt: "Rails logo"),
+            'http://rubyonrails.org/'
+  end
 end
